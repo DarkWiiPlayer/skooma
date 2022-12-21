2b90316ac62ee25d58a0f44958198d67680411d7
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-12-21 13:13:26 +0100

    Update spooder tasks to new semantics

--- tasks.lua
+++ tasks.lua
@@ -10,36 +10,30 @@ spooder.task "install" {
 
 spooder.task "test" {
 	description = "Runs tests";
-	function(helper)
-		helper.run([[
-			rm luacov.stats.out
-			luacheck . || exit
-			busted --coverage --lpath '?/init.lua;?.lua' || exit
-			luacov -r html skooma.lua
-		]])
-	end
+	'rm luacov.stats.out || true';
+	'luacheck .';
+	'busted --coverage --lpath "?/init.lua;?.lua"';
+	'luacov -r html skooma.lua';
 }
 
 spooder.task "documentation" {
 	description = "Builds and pushes the documentation";
 	depends = { "test"};
-	function(helper)
-		helper.run([[
-			hash=$(git log -1 --format=%h)
-			mkdir -p doc/coverage
-			cp -r luacov-html/* doc/coverage
-			rm -r doc/*
-			ldoc .
-			cd doc
-				find . | treh -c
-				git add --all
-				if git log -1 --format=%s | grep "$hash$"
-				then git commit --amend --no-edit
-				else git commit -m "Update documentation to $hash"
-				fi
-				git push --force origin doc
-			cd ../
-			git stash pop || true
-		]])
-	end;
+	[[
+		hash=$(git log -1 --format=%h)
+		mkdir -p doc/coverage
+		cp -r luacov-html/* doc/coverage
+		rm -r doc/*
+		ldoc .
+		cd doc
+			find . | treh -c
+			git add --all
+			if git log -1 --format=%s | grep "$hash$"
+			then git commit --amend --no-edit
+			else git commit -m "Update documentation to $hash"
+			fi
+			git push --force origin doc
+		cd ../
+		git stash pop || true
+	]]
 }
